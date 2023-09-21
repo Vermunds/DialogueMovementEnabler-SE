cb774cc1e5a8055eb341d477bc5eec291598729a
Vermunds <33235701+Vermunds@users.noreply.github.com>
2023-09-21 18:50:09 +0200

    Update project

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ extern "C" {
 	DLLEXPORT SKSE::PluginVersionData SKSEPlugin_Version = []() {
 		SKSE::PluginVersionData v{};
 		v.PluginVersion(REL::Version{ Version::MAJOR, Version::MINOR, Version::PATCH, 0 });
-		v.PluginName(Version::PROJECT);
-		v.AuthorName("Vermunds"sv);
-		v.CompatibleVersions({ SKSE::RUNTIME_1_6_318 });
+		v.PluginName(Version::NAME);
+		v.AuthorName(Version::AUTHOR);
+		v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
 
 		v.addressLibrary = true;
 		v.sigScanning = false;
@@ -18,7 +18,7 @@ extern "C" {
 	DLLEXPORT bool SKSEPlugin_Load(SKSE::LoadInterface* a_skse)
 	{
 		assert(SKSE::log::log_directory().has_value());
-		auto path = SKSE::log::log_directory().value() / std::filesystem::path("DialogueMovementEnabler.log");
+		auto path = SKSE::log::log_directory().value() / std::filesystem::path(Version::NAME.data() + ".log"s);
 		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path.string(), true);
 		auto log = std::make_shared<spdlog::logger>("global log", std::move(sink));
 
@@ -28,14 +28,7 @@ extern "C" {
 		spdlog::set_default_logger(std::move(log));
 		spdlog::set_pattern("%g(%#): [%^%l%$] %v", spdlog::pattern_time_type::local);
 
-		SKSE::log::info("Dialogue Movement Enabler v" + std::string(Version::NAME) + " - (" + std::string(__TIMESTAMP__) + ")");
-
-		if (a_skse->IsEditor())
-		{
-			SKSE::log::critical("Loaded in editor, marking as incompatible!");
-			return false;
-		}
-
+		SKSE::log::info("{} v{} -({})", Version::FORMATTED_NAME, Version::STRING, __TIMESTAMP__);
 		SKSE::Init(a_skse);
 
 		DME::LoadSettings();
